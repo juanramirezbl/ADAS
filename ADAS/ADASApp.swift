08823c46b5e5c8f02ea6c518d685cb6129690a0c
juanramirezbl <najublanajubla@gmail.com>
2025-11-10 14:32:41 +0100

    Initial Commit

---
+++ ADAS/ADASApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADASApp.swift
+//  ADAS
+//
+//  Created by Juan Ramirez Blanco on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ADASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADASTests/ADASTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADASTests.swift
+//  ADASTests
+//
+//  Created by Juan Ramirez Blanco on 10/11/25.
+//
+
+import Testing
+@testable import ADAS
+
+struct ADASTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
