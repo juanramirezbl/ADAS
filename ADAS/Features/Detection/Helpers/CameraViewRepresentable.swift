392e38ff73785ed393f0d03d19b2d19fa4021656
juanramirezbl <najublanajubla@gmail.com>
2025-11-25 11:34:35 +0100

    feat: No comments

--- ADAS/Features/Detection/Helpers/CameraViewRepresentable.swift
+++ ADAS/Features/Detection/Helpers/CameraViewRepresentable.swift
@@ -5,12 +5,12 @@ import UIKit
 // This struct is the "bridge" between SwiftUI and UIKit
 struct CameraViewRepresentable: UIViewControllerRepresentable {
     
-    // 1. This creates the UIKit view controller
+    // 1.the UIKit view controller
     func makeUIViewController(context: Context) -> RealTimeViewController {
         return RealTimeViewController()
     }
     
-    // 2. This is for passing data from SwiftUI TO UIKit
+    // 2.passing data from SwiftUI TO UIKit
     func updateUIViewController(_ uiViewController: RealTimeViewController, context: Context) {
         
     }
